c61983643035974de8a8910b2b4a57dc9c70af0c
Vladislav Svitkovskiy <svitkovskiy198@gmail.com>
2021-02-09 08:32:49 +0300

    added phone and address to fields group in WP

--- header.php
+++ header.php
@@ -44,14 +44,13 @@
                     <div class="header__contacts-item">
                         <img src="<?php echo bloginfo('template_url');?>/assets/img/icons/svg/phone.svg" alt="телефон" class="header__contacts-logo">
                         <div class="header__contacts-tel">
-                            <a href="tel:+797867834347">+797867834347</a>
-                            <a href="tel:+797867834358">+797867834358</a>
+                            <a href="<?php the_field('phone_1', 2); ?>"><?php the_field('phone_1', 2); ?></a> <!-- добавление телефона из админ панели (цифра 2 - id главной страницы, можно использовать одну группу полей с условием "если страница главная")-->
+                            <a href="<?php the_field('phone_2', 2); ?>"><?php the_field('phone_2', 2); ?></a>
                         </div>
                     </div>
                     <div class="header__contacts-item">
                         <img src="<?php echo bloginfo('template_url');?>/assets/img/icons/svg/pointer.svg" alt="указатель" class="header__contacts-logo">
-                        <address>ул. Василисы Кожиной, 1<br>
-                            Москва, Россия, 121096</address>
+                        <address><?php the_field('address', 2); ?></address>
                     </div>
                 </div>
             </div>
